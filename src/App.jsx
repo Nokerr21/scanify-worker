7ef92c9c300bfeb2248bdaa6fc307774bf127a05
wkondrac <01158943@pw.edu.pl>
2023-10-17 17:06:28 +0200

    add get size of tag in bytes v1.7.7

--- src/App.jsx
+++ src/App.jsx
@@ -140,7 +140,7 @@ export default function App(){
           } catch(error) {
             consoleLogWrite(error.name);
             if(error.name == 'NetworkError'){
-              consoleLogWriteTest(error.name.message)
+              consoleLogWriteTest(error.options)
             }
             setMessage("")
           }
