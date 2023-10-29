5556a958e5a29ca449a712517772c438c901c73c
wkondrac <01158943@pw.edu.pl>
2023-10-29 19:20:17 +0100

    small qr scanner fix

--- src/components/Qrs/RenderQrScanner/RenderQrScanner.jsx
+++ src/components/Qrs/RenderQrScanner/RenderQrScanner.jsx
@@ -1,31 +1,31 @@
 import { Html5QrcodeScanner } from "html5-qrcode";
 import { logQrScanRes } from "./LogQrScanRes";
+import { sleep } from "../../Nfcs/WriteTag/Sleep";
 
 export function renderQrScanner( setQrs, setQrScanResult, setQrScanTime ){
     const html5QrcodeScanner = new Html5QrcodeScanner("readerQR", { fps: 5, qrbox: 250 });
   
-    html5QrcodeScanner.render(onScanSuccess, onScanError);
-    console.log(html5QrcodeScanner.getState()  + " drugi");
-  
-    var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ":" + today.getMilliseconds();
-    var dateTime = date+' '+time;
-            
-    function onScanSuccess(decodedText, decodedResult) {
+    html5QrcodeScanner.render(onScanSuccess);
+
+    async function onScanSuccess(decodedText, decodedResult) {
+        var today = new Date();
+        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ":" + today.getMilliseconds();
+        var dateTime = date + ' ' + time;
         console.log(`Scan result: ${decodedText}`, decodedResult);
         setQrScanResult(decodedText);
-        setQrScanTime(dateTime);
+        //setQrScanTime(dateTime);
         logQrScanRes("Message: '" + decodedText + "' decoded!" + "\n" + "TimeStamp: " + dateTime);
-        console.log(decodedText.getBytes)
         if (decodedText != ""){
             setQrs((currentQRs) => {
               return [... currentQRs, {id: crypto.randomUUID(), title: decodedText, completed: false}, ]
             })
         }
-        console.log(html5QrcodeScanner.getState()  + " trzeci");
-        html5QrcodeScanner.clear();
+        
+        //html5QrcodeScanner.clear();
+        html5QrcodeScanner.pause();
+        await sleep(700);
+        html5QrcodeScanner.resume();
+        
     }
-  
-    function onScanError(err){}
 }
