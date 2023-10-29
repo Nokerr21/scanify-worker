1563a46012bedb44c6c88bdfd0029aba89143c74
wkondrac <01158943@pw.edu.pl>
2023-10-29 13:08:33 +0100

    small fixes

--- src/components/Qrs/QrScannerComponent.jsx
+++ src/components/Qrs/QrScannerComponent.jsx
@@ -1,9 +1,13 @@
-export default function QrScannerComponent() {
-    return (
-        <div className="form-row">
-            <label>READ QR CODE</label>
-            <div id="readerQR"></div>
-            <pre className="log" id="logQrScanRes"></pre>
-        </div>
-    );
+import React from "react";
+
+export default class QrScannerComponent extends React.Component {
+    render () {
+        return (
+            <div className="form-row">
+                <label>READ QR CODE</label>
+                <div id="readerQR"></div>
+                <pre className="log" id="logQR"></pre>
+            </div>
+        );
+    }
 }
