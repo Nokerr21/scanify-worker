6696f3948780a58f151b630f2a40997eebcd632a
wkondrac <01158943@pw.edu.pl>
2023-11-10 14:30:31 +0100

    add .env file

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-export const instance = axios.create({
+export default instance = axios.create({
     baseURL: process.env.API_URL || 'https://node-nfc-db.onrender.com/api'
 });
