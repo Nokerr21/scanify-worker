1d9d4fb2744e75979f3954a47e8490ef0bd8e533
wkondrac <01158943@pw.edu.pl>
2023-11-13 17:18:01 +0100

    change read function

--- src/components/Nfcs/ReadTag/ReadTag.jsx
+++ src/components/Nfcs/ReadTag/ReadTag.jsx
@@ -1,26 +1,52 @@
 import logReadTag from "./LogReadTag";
-
+import axios from "../../../axios";
 
 export default async function readTag() {
   if ("NDEFReader" in window) {
-    const ndefReader = new NDEFReader();
+    const ndef = new NDEFReader();
     try {
-      await ndefReader.scan();
-      ndefReader.onreading = event => {
-        const decoder = new TextDecoder();
-        for (const record of event.message.records) {
-          var today = new Date();
-          var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-          var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ":" + today.getMilliseconds();
-          var dateTime = date+' '+time;
-          logReadTag("---- data ----\n" + decoder.decode(record.data) + "\n" + "TimeStamp: " + dateTime);
+      return new Promise((resolve, reject) => {
+        const abortContr = new AbortController();
+        abortContr.signal.onabort = reject;
+        ndef.addEventListener("reading", event => {
+          abortContr.abort();
+          resolve(event);
+        }, { once: true });
+        ndef.scan({ signal: abortContr.signal }).catch(err => reject(err));
+        ndef.onreading = event => {
+          const decoder = new TextDecoder();
+          for (const record of event.message.records) {
+            var today = new Date();
+            var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ":" + today.getMilliseconds();
+            var dateTime = date + ' ' + time;
+            axios.get('/nfcs/' + decoder.decode(record.data)).then(function(result){
+              if(result.data.batchNumber != undefined){
+                logReadTag("Product information:\n" + result.data.info.toString() + "\n" + "Index: " + result.data.index.toString() +
+                "\n" + "Batch number: " + result.data.batchNumber.toString() + "\n" + "TimeStamp: " + dateTime);
+              }
+              else{
+                logReadTag("Product information:\n" + result.data.info.toString() + "\n" + "Index: " + result.data.index.toString() +
+                "\n" + "TimeStamp: " + dateTime);
+              }
+              
+            });
+          }
         }
-      }
+      }).catch(err => {
+        console.log("NFC reading stopped...");
+      });
+      
     } catch(error) {
-      logReadTag(error);
+      if (error instanceof Error && error.name === 'AbortError') {
+        // work time expired, just return
+      }
+      else{
+        logReadTag(error);
+      }
+      
     }
-  } 
-  else {
+  } else {
     logReadTag("Web NFC is not supported.");
   }
 }
