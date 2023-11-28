ebc7e24f75b8e71c7c0ded7a659aff1e99ef7151
wkondrac <01158943@pw.edu.pl>
2023-11-28 13:00:42 +0100

    change ui v4.0

--- src/components/Nfcs/WriteTagComponent.jsx
+++ src/components/Nfcs/WriteTagComponent.jsx
@@ -6,8 +6,11 @@ export function WriteTagComponent( { QrScanResult } ) {
     return (
         <div className="row-btn-write">
             <button onClick={() => writeTag(QrScanResult)} className="btn-write" id="btn-write">Write QR to NFC</button>
-            <pre className="log-write" id="logWriteTagTest"></pre>
-            <pre className="log-write" id="logWriteTag"></pre>
+            <div className="log-write-div">
+                <pre className="log-write" id="logWriteTagTest"></pre>
+                <pre className="log-write" id="logWriteTag"></pre>
+            </div>
+            
         </div>
     );
 }
