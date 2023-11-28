845f2e3070f1c58417d051c73b3578eb7ec417a0
wkondrac <01158943@pw.edu.pl>
2023-11-28 14:59:42 +0100

    change ui v4.7

--- src/components/Nfcs/ReadTagComponent.jsx
+++ src/components/Nfcs/ReadTagComponent.jsx
@@ -5,7 +5,7 @@ import "./ReadTagComponent.css";
 export function ReadTagComponent() {
     return (
         <div className="row">
-            <label className="labl">READ NFC</label>
+            <label className="labl-read">READ NFC</label>
             <button onClick={() => readTag()} className="btn">Read</button>
             <div className="log-read-div">
                 <pre className="log-test" id="logReadTagTest"></pre>
