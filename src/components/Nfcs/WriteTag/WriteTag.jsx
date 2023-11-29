cee0a739222337182c517a3bcbe0babe2ba2e780
wkondrac <01158943@pw.edu.pl>
2023-11-29 17:37:11 +0100

    change ui v8.0

--- src/components/Nfcs/WriteTag/WriteTag.jsx
+++ src/components/Nfcs/WriteTag/WriteTag.jsx
@@ -7,7 +7,9 @@ import axios from "../../../axios";
 export default async function writeTag(message, batchNumber, times = 2) {
   var checkBox = document.getElementById("batchCheck");
   if ("NDEFReader" in window) {
-    logWriteTag("");
+    if (times < 2) {
+      logWriteTag("");
+    }
     const ndef = new NDEFReader();
     try {
       var today = new Date();
