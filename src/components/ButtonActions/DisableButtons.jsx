cdbe4e83a478791cdf5c15121cf28a31021e180b
wkondrac <01158943@pw.edu.pl>
2023-11-29 14:14:45 +0100

    change ui v5.8

--- src/components/ButtonActions/DisableButtons.jsx
+++ src/components/ButtonActions/DisableButtons.jsx
@@ -1,11 +1,9 @@
 export function disableButtons(){
-    var buttonsDelete = document.querySelectorAll('ul.list-qrs button.btn-delete')
-    var buttonsWrite = document.querySelectorAll('ul.list-qrs button.btn-list-write')
+    var buttonsDelete = document.querySelectorAll('ul.list-qrs button')
     var checkBox = document.getElementById("batchCheck");
     var buttonInDiv = document.getElementById("btn-write");
     if(checkBox.checked == true) {
       buttonsDelete.forEach((button) => {button.disabled = true;});
-      buttonsWrite.forEach((button) => {button.disabled = true;});
       buttonInDiv.disabled = true;
       console.log("checked")
     }
--- src/components/ButtonActions/EnableButtons.jsx
+++ src/components/ButtonActions/EnableButtons.jsx
@@ -1,9 +1,9 @@
 export function enableButtons(){
-    var buttonsDelete = document.querySelectorAll('ul.list-qrs button.btn-delete')
-    var buttonsWrite = document.querySelectorAll('ul.list-qrs button.btn-list-write')
+    var buttonsDelete = document.querySelectorAll('ul.list-qrs button')
+  
     var buttonInDiv = document.getElementById("btn-write");
     buttonsDelete.forEach((button) => {button.disabled = false;});
-    buttonsWrite.forEach((button) => {button.disabled = false;});
+ 
     buttonInDiv.disabled = false;
     console.log('enable');
 }
