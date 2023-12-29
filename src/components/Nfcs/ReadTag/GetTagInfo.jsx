9d651a98cdfba53c2d294328860ee04518725522
wkondrac <01158943@pw.edu.pl>
2023-12-29 15:11:47 +0100

    code formatting v1.0

---
+++ src/components/Nfcs/ReadTag/GetTagInfo.jsx
@@ -0,0 +1,46 @@
+import logReadTag from "./LogReadTag";
+import axios from "../../../axios";
+import logReadTagTest from "./LogReadTagTest"
+import { getDateAndTime } from "../../Date/GetDateAndTime";
+
+export default async function getTagInfo(event) {
+  const decoder = new TextDecoder();
+  logReadTagTest("Reading tag... Step[2/3]");
+  for (const record of event.message.records) {
+    var dateTime = getDateAndTime();
+    axios.get('/nfcs/' + decoder.decode(record.data)).then(function(result){
+      if(result.data.batchNumber != undefined){
+        logReadTagTest("Success!");
+        if(result.data.index != undefined) {
+          logReadTag("Product information:\n" + result.data.info.toString() + "\nIndex: " + result.data.index.toString() +
+          "\nBatch number: " + result.data.batchNumber.toString() + "\nTagged at: " + result.data.timeStamp.toString() +
+          "\nRead at: " + dateTime);
+        }
+        else {
+          logReadTag("Product information:\n" + result.data.info.toString() + "\nBatch number: " +
+          result.data.batchNumber.toString() + "\nTagged at: " + result.data.timeStamp.toString() + "\nRead at: " + dateTime);
+        }
+      }
+      else{
+        logReadTagTest("Success!");
+        if(result.data.index != undefined) {
+          logReadTag("Product information:\n" + result.data.info.toString() + "\nIndex: " + result.data.index.toString() +
+          "\nTagged at: " + result.data.timeStamp.toString() + "\nRead at: " + dateTime);
+        }
+        else {
+          logReadTag("Product information:\n" + result.data.info.toString() + "\nTagged at: " +
+          result.data.timeStamp.toString() + "\nRead at: " + dateTime);
+        }
+      }
+    }).catch(err => {
+        if (err.name == 'TypeError') {
+          logReadTagTest("Oops!");
+          logReadTag("This tag is no longer in our database.");
+        }
+        else {
+          logReadTagTest("This tag is not defined in our database.");
+          logReadTag('Message saved on this tag:\n' + decoder.decode(record.data));
+        }
+      });
+  }    
+}
--- src/components/Nfcs/ReadTagComponent.jsx
+++ src/components/Nfcs/ReadTagComponent.jsx
@@ -1,12 +1,13 @@
 import readTag from "./ReadTag/ReadTag";
 import "./ReadTagComponent.css";
+import readTagTest from "./ReadTag/ReadTagTest";
 
 
 export function ReadTagComponent() {
     return (
         <div className="row">
             <label className="labl-read">READ NFC</label>
-            <button onClick={() => readTag()} className="btn">Read</button>
+            <button onClick={() => readTagTest()} className="btn">Read</button>
             <div className="log-read-div">
                 <pre className="log-test" id="logReadTagTest"></pre>
                 <pre className="log" id="logReadTag"></pre>
